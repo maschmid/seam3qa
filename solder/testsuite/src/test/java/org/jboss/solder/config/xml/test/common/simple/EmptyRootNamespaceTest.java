8b2edfa622b64e0670f5f2add04b71a8736fb6e6
Marek Schmidt <maschmid@redhat.com>
2012-12-10 13:50:32 +0100

    workaround for shrinkwrap having more deployment methods in a subclass

--- solder/testsuite/src/test/java/org/jboss/solder/config/xml/test/common/simple/EmptyRootNamespaceTest.java
+++ solder/testsuite/src/test/java/org/jboss/solder/config/xml/test/common/simple/EmptyRootNamespaceTest.java
@@ -2,12 +2,20 @@ package org.jboss.solder.config.xml.test.common.simple;
 
 import static org.jboss.solder.config.xml.test.common.util.Deployments.baseDeployment;
 
+import java.lang.reflect.Method;
+import java.util.Set;
+
+import javax.enterprise.inject.spi.Bean;
+import javax.enterprise.inject.spi.BeanManager;
+import javax.inject.Inject;
+
+import junit.framework.Assert;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.junit.runner.RunWith;
-
+import org.junit.Test;
 
 /**
  * This test verifies that a no-namespace root element does not break the deployment.
@@ -16,7 +24,7 @@ import org.junit.runner.RunWith;
  * @see https://issues.jboss.org/browse/SEAMXML-45
  */
 @RunWith(Arquillian.class)
-public class EmptyRootNamespaceTest extends SimpleBeanTest {
+public class EmptyRootNamespaceTest {
     @Deployment(name = "EmptyRootNamespaceTest")
     public static Archive<?> deployment() {
         return baseDeployment(EmptyRootNamespaceTest.class, "empty-root-namespace-beans.xml")
@@ -25,5 +33,48 @@ public class EmptyRootNamespaceTest extends SimpleBeanTest {
                     Bean1.class, Bean2.class, Bean3.class, 
                     ExtendedBean.class, ExtendedQualifier1.class, ExtendedQualifier2.class,
                     OverriddenBean.class, ScopeOverrideBean.class);
+    }
+
+    @Inject
+    Bean1 x;
+
+    @Inject
+    Bean2 bean2;
+
+    @Inject
+    Bean3 y;
+
+    @Test
+    public void simpleBeanTest() {
+        Assert.assertTrue(x != null);
+        Assert.assertTrue(x.bean2 != null);
+
+        Assert.assertEquals("test value", bean2.produceBean3);
+
+        Assert.assertTrue(y != null);
+        Assert.assertTrue("Post construct method not called", x.value == 1);
+    }
+
+    @Inject
+    BeanManager manager;
+
+    @Test
+    public void testOverride() {
+        Set<Bean<?>> beans = manager.getBeans(OverriddenBean.class);
+        Assert.assertTrue(beans.size() == 1);
+        Assert.assertTrue(beans.iterator().next().getName().equals("someBean"));
+
+    }
+
+    @Inject
+    @ExtendedQualifier1
+    @ExtendedQualifier2
+    ExtendedBean ext;
+
+    @Test
+    public void testExtends() throws SecurityException, NoSuchMethodException {
+        Assert.assertTrue(ext != null);
+        Method method = ext.getClass().getDeclaredMethod("getData");
+        method.getGenericReturnType();
     }
 }
